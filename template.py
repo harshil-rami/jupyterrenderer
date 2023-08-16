2fdfb8d84ba8b9fa348af6c3eb98cc7797dbd7a0
harshil-rami <ramiharshil2598@gmail.com>
2023-08-16 23:03:29 +0530

    base skeletong

---
+++ template.py
@@ -0,0 +1,46 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s: %(levelname)s]: %(message)s"
+)
+
+while True:
+    project_name = input("Enter the Project Name : ")
+    if project_name != '':
+        break
+
+if project_name:
+    logging.info(f"Creating the project by name: {project_name}")
+else:
+    logging.warning("Project name is not initialized")
+
+
+# list of files:
+list_of_files = [
+    ".github/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"tests/__init__.py",
+    f"tests/unit/__init__.py",
+    f"tests/intregration/__init__.py",
+    "init_setup.sh",
+    "requirements.txt",
+    "requirements_dev.txt",
+    "setup.py",
+    "pyproject.toml",
+    "setup.cfg",
+    "tox.ini"
+]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir,filename = os.path.split(filepath)
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating a directory at : {filedir} for file: {filename}")
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            logging.info(f"Creating a new file: {filename} at path: {filepath}")
+    
